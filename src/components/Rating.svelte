67c7e9768221742af8c75b42735566512de69ce3
reshapuspita <reshapuspita734@gmail.com>
2024-06-20 11:02:47 +0700

    feat: rating component

--- src/components/Rating.svelte
+++ src/components/Rating.svelte
@@ -1,1 +1,31 @@
-<!-- Your code here -->
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  let selected = 1;
+
+  const dispatch = createEventDispatcher();
+
+  const onChange = (event) => {
+    selected = event.currentTarget.value;
+    dispatch("rating-select", selected);
+  };
+
+  const range = (start, end) => {
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  }
+</script>
+
+<ul class="rating">
+  {#each range(1, 10) as i}
+    <li>
+      <input
+        type="radio"
+        id="num{i}"
+        value={i}
+        on:change={onChange}
+        checked={selected === i}
+      />
+      <label for="num{i}">{i}</label>
+    </li>
+  {/each}
+</ul>
